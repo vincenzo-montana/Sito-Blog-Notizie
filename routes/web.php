19b6113731c7ace4fa6b9122eedb35f0af637a94
Emanuele-Morelli <emanuele1996.m@gmail.com>
2024-10-03 21:43:39 +0200

    Rotta delete dell'admin delete category aggiustata

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,8 @@ Route::middleware('admin')->group(function(){
 
     //Rotte Dashboard admin edit o delete delle categorie
     Route::put('/admin/edit/category/{category}', [AdminController::class, 'editCategory'])->name('admin.editCategory');
-    Route::put('/admin/delete/category/{category}', [AdminController::class, 'deleteCategory'])->name('admin.deleteCategory');
+    Route::delete('/admin/delete/category/{category}', [AdminController::class, 'deleteCategory'])->name('admin.deleteCategory');
+    
     //rotta per la creazione  di nuove categorie
     Route::post('/admin/category/store', [AdminController::class, 'storeCategory'])->name('admin.storeCategory');
 });
